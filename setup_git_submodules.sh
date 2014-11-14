0fe12c00cd750bc2c404e8b176ba4e14080ab59c
Louis Plissonneau <louis.plissonneau@groupon.com>
2014-11-14 14:54:36 +0000

    updated submodules

--- setup_git_submodules.sh
+++ setup_git_submodules.sh
@@ -2,5 +2,6 @@
 git submodule init
 git submodule update
 git submodule foreach git submodule init
-git submodule foreach git submodule update
+git submodule foreach git pull origin master
+#git submodule foreach git submodule update
 
